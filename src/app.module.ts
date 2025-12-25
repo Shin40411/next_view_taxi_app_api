3132cce7bd6193545c12025d66a6f8b1cce69107
Shin40411 <dtho961@gmail.com>
2025-12-25 23:41:15 +0700

    add custom port into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,6 +30,7 @@ import { VietmapService } from './utils/vietmap.service';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST!,
+      port: Number(process.env.DB_PORT) || 3306,
       username: process.env.DB_USERNAME!,
       password: process.env.DB_PASSWORD!,
       database: process.env.DB_NAME || 'taxi_app_db',
